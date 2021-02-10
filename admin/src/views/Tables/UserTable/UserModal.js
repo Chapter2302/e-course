615ac27a156a92df5c6aec5135bb52ab2a464347
Duy Chuong <chapter2302@gmail.com>
2021-02-10 23:07:36 +0700

    update code

---
+++ admin/src/views/Tables/UserTable/UserModal.js
@@ -0,0 +1,298 @@
+import { Modal } from "react-bootstrap";
+import ReactStars from "react-rating-stars-component";
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    Container,
+    Row,
+    Col,
+    CardFooter,
+  } from "reactstrap";
+
+const DefaultAvatar = 'https://maytinhquanganh.com/images/noavatar.jpg';
+
+const UserModal = (props) => {
+    const user  = props.user;
+    const isEdit = props.isEdit;
+
+    return(
+        !props.user ? <></> : 
+        <Modal {...props}  size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+            <Modal.Header><p></p></Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+                        <Card className="card-profile shadow">
+                            <Row className="justify-content-center">
+                                <Col className="order-lg-2" lg="3">
+                                    <div className="card-profile-image">
+                                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                                        <img
+                                            alt="..."
+                                            className="rounded-circle"
+                                            width="180px" height="180px"
+                                            style={{backgroundColor: "rgb(173,181,189)"}}
+                                            src={user.photoUser ? user.photoUser : DefaultAvatar}
+                                        />
+                                        </a>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                                <div className="d-flex justify-content-between"></div>
+                            </CardHeader>
+                            <CardBody className="pt-0 pt-md-4">
+                                <Row>
+                                    <div className="col mt-4">
+                                        <div className="card-profile-stats d-flex justify-content-center mt-md-5">
+                                            <input className="btn btn-primary" type="file"/>
+                                        </div>
+                                        <div className="d-flex justify-content-center">
+                                            <ReactStars
+                                                count={5}
+                                                // onChange={ratingChanged}
+                                                size={30}
+                                                emptyIcon={<i className="far fa-star"></i>}
+                                                fullIcon={<i className="fas fa-star"></i>}
+                                                activeColor="#F5365C"
+                                            />
+                                        </div>
+                                    </div>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <Row className="d-flex justify-content-center">
+                                    <button className="btn btn-lg btn-primary">CONFIRM</button>{' '}
+                                    <button className="btn btn-lg btn-secondary" onClick={props.onHide}>CANCEL</button>
+                                </Row>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col className="order-xl-1" xl="8">
+                        <Card className="bg-secondary shadow">
+                        <CardBody>
+                            <Form>
+                            <h6 className="heading-small text-muted mb-4">
+                                User information
+                            </h6>
+                            <div className="pl-lg-4">
+                                <Row>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-username"
+                                        >
+                                            Username
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            defaultValue="lucky.jesse"
+                                            id="input-username"
+                                            placeholder="Username"
+                                            type="text"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-email"
+                                        >
+                                            Email address
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            id="input-email"
+                                            placeholder="jesse@example.com"
+                                            type="email"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                            <label
+                                                className="form-control-label"
+                                                htmlFor="input-city"
+                                            >
+                                                Date Of Birth
+                                            </label>
+                                            <Input
+                                                className="form-control-alternative"
+                                                type="date"
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                            <label
+                                                className="form-control-label"
+                                                htmlFor="input-last-name"
+                                            >
+                                                Password
+                                            </label>
+                                            <Input
+                                                className="form-control-alternative"
+                                                placeholder="Password"
+                                                type="password"
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-username"
+                                        >
+                                            Bank ID
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            placeholder="0987-6543-210"
+                                            type="text"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col lg="6">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            type="text"
+                                        >
+                                            Phone Number
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            placeholder="0987654321"
+                                            type="email"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                            </div>
+                            <div className="pl-lg-4">
+                                <Row>
+                                    <Col md="12">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-address"
+                                        >
+                                            Workplace
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                                            placeholder="Workplace Address"
+                                            type="text"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md="12">
+                                        <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-address"
+                                        >
+                                            Address
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                                            placeholder="Home Address"
+                                            type="text"
+                                        />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                <Col lg="4">
+                                    <FormGroup>
+                                        <label
+                                            className="form-control-label"
+                                            htmlFor="input-city"
+                                        >
+                                            Balance
+                                        </label>
+                                        <Input
+                                            className="form-control-alternative"
+                                            placeholder="300"
+                                            type="number"
+                                        />
+                                    </FormGroup>
+                                </Col>
+                                <Col lg="4">
+                                    <FormGroup>
+                                    <label
+                                        className="form-control-label"
+                                        htmlFor="input-country"
+                                    >
+                                        Gender
+                                    </label>
+                                    <Input
+                                        className="form-control-alternative"
+                                        type="select"
+                                    >   
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                    </Input>
+                                    </FormGroup>
+                                </Col>
+                                <Col lg="4">
+                                    <FormGroup>
+                                    <label
+                                        className="form-control-label"
+                                        htmlFor="input-country"
+                                    >
+                                        Role
+                                    </label>
+                                    <Input
+                                        className="form-control-alternative"
+                                        type="select"
+                                        id="input-postal-code"
+                                    >   
+                                        <option value="student">Student</option>
+                                        <option value="teacher">Teacher</option>
+                                        <option value="admin">Admin</option>
+                                    </Input>
+                                    </FormGroup>
+                                </Col>
+                                </Row>
+                            </div>
+                            <div className="pl-lg-4">
+                                <FormGroup>
+                                    <label>Biology</label>
+                                <Input
+                                    className="form-control-alternative"
+                                    placeholder="A few words about you ..."
+                                    rows="4"
+                                    defaultValue="A beautiful Dashboard for Bootstrap 4. It is Free and
+                                    Open Source."
+                                    type="textarea"
+                                />
+                                </FormGroup>
+                            </div>
+                            </Form>
+                        </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+        </Modal>
+    )
+}
+
+export default UserModal;
---
+++ admin/src/views/Tables/UserTable/index.js
@@ -0,0 +1,232 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useEffect, useState } from "react";
+// reactstrap components
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  Container,
+  Row,
+  UncontrolledTooltip,
+} from "reactstrap";
+// core components
+import Header from "components/Headers/Header.js";
+import { getAll } from "api"
+
+const DefaultAvatar = 'https://maytinhquanganh.com/images/noavatar.jpg';
+
+const Tables = () => {
+  const [userList, setUserList] = useState([]);
+  const [renderUsers, setRenderUsers] = useState([]);
+  const [pages, setPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const renderPaginationItems = () => {
+    let paginationItems = [];
+
+    for(let index=1; index<=pages; index++) {
+      paginationItems.push(
+        <PaginationItem key={ 'pagination-item-' + index } className={ currentPage == index ? "active" : "" }>
+          <PaginationLink
+            onClick={
+              (e) => { 
+                e.preventDefault();
+                setCurrentPage(index);
+                setRenderUsers(userList.slice((index - 1) * 10, index * 10));
+              }
+            }
+          >
+            { index }
+          </PaginationLink>
+        </PaginationItem>
+      )
+    }
+
+    return paginationItems;
+  }
+
+  useEffect(() => {
+    getAll('user').then(res => { 
+      setUserList(res);
+      setPages(res.length % 10 == 0 ? Math.floor(res.length / 10) : Math.floor(res.length / 10) + 1);
+      setRenderUsers(res.slice((currentPage - 1) * 10, currentPage * 10));
+    })
+  }, [])
+
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          {
+            renderUsers.length == 0 
+            ? (<div className="col">
+              Waiting For Data
+            </div>)
+            : (<div className="col">
+              <Card className="shadow">
+                <CardHeader className="border-0">
+                  <h3 className="mb-0">Card tables</h3>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col">Photo</th>
+                      <th scope="col">Fullname</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Gender</th>
+                      <th scope="col">Role</th>
+                      <th scope="col" />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      renderUsers.map((user, index) => {
+                        return(
+                          <tr key={'user_' + index}>
+                            <th scope="row">
+                              <Media className="align-items-center">
+                                <a
+                                  className="avatar rounded-circle mr-3"
+                                  href="#"
+                                  onClick={(e) => e.preventDefault()}
+                                >
+                                  <img
+                                    style={{height: "100%"}}
+                                    alt="..."
+                                    src={user.photoUser ? user.photoUser : DefaultAvatar}
+                                  />
+                                </a>
+                              </Media>
+                            </th>
+                            <td>{user.fullName}</td>
+                            <td>
+                              {user.authenticateMethod.local.email || user.authenticateMethod.google.email}
+                            </td>
+                            <td>
+                              {user.sex}
+                            </td>
+                            <td>
+                              {(user.role)[0].toUpperCase() + (user.role).slice(1)}
+                            </td>
+                            <td className="text-right">
+                              <UncontrolledDropdown>
+                                <DropdownToggle
+                                  className="btn-icon-only text-light"
+                                  href="#pablo"
+                                  role="button"
+                                  size="sm"
+                                  color=""
+                                  onClick={(e) => e.preventDefault()}
+                                >
+                                  <i className="fas fa-ellipsis-v" />
+                                </DropdownToggle>
+                                <DropdownMenu className="dropdown-menu-arrow" right>
+                                  <DropdownItem
+                                    onClick={}
+                                  >
+                                    Detail
+                                  </DropdownItem>
+                                  <DropdownItem
+                                    onClick={(e) => e.preventDefault()}
+                                  >
+                                    Edit
+                                  </DropdownItem>
+                                  <DropdownItem
+                                    href="#pablo"
+                                    onClick={(e) => e.preventDefault()}
+                                  >
+                                    Delete
+                                  </DropdownItem>
+                                </DropdownMenu>
+                              </UncontrolledDropdown>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+                </Table>
+                <CardFooter className="py-4">
+                  <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className={ currentPage == 1 ? "disabled" : "" }>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={
+                          (e) => { 
+                            e.preventDefault();
+                            const newCurrentPage = currentPage == 1 ? currentPage : currentPage - 1
+                            setCurrentPage(newCurrentPage)
+                            setRenderUsers(userList.slice((newCurrentPage - 1) * 10, newCurrentPage * 10));
+                          }
+                        }
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    { renderPaginationItems() }
+                    {console.log(currentPage, pages)}
+                    <PaginationItem className={ currentPage == pages ? "disabled" : "" }>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={
+                          (e) => { 
+                            e.preventDefault();
+                            const newCurrentPage = currentPage == pages ? currentPage : currentPage + 1
+                            setCurrentPage(newCurrentPage)
+                            setRenderUsers(userList.slice((newCurrentPage - 1) * 10, newCurrentPage * 10));
+                          }
+                        }
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                  </nav>
+                </CardFooter>
+              </Card>
+            </div>)
+          }
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Tables;
